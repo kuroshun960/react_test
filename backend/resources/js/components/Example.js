294bca7ef5ff90589dc91740405023cffa561632
kurosaki_shunsuke <kurosaki_shunsuke@oisixradaichi.co.jp>
2021-06-22 18:01:10 +0900

    Material-UI　インストール

--- backend/resources/js/components/Example.js
+++ backend/resources/js/components/Example.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Button } from '@material-ui/core'; //Buttonをインポート
+
+
+// Viewのappに、書き出す内容。
+
 function Example() {
     return (
         <div className="container">
@@ -10,12 +15,18 @@ function Example() {
                         <div className="card-header">Example Component</div>
 
                         <div className="card-body">I'm an example component!</div>
+
+                        <Button color="primary" variant="contained">Hello World</Button> 
+                        
                     </div>
                 </div>
             </div>
         </div>
     );
 }
+
+
+// 以下の記述でViewのappに、Example.jsの内容↑が書き出される。
 
 export default Example;
 
